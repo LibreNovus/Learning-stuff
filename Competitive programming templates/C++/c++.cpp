f6692540e2721e66155b607485c9cc9373634774
Kim Petersen <vrchat2009@gmail.com>
2020-12-07 17:13:39 +0100

    CPP templete

---
+++ Competitive programming templates/C++/c++.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	#ifndef ONLINE_JUDGE
+		// Get input from input.txt
+	    freopen("input.txt", "r", stdin);
+	   	// Writing output to output.txt
+	    freopen("output.txt", "w", stdout);
+	#endif
+
+	// Computation
+
+	int a,b;
+
+	cin>>a>>b;
+
+	cout<<(a*b);
+
+	return 0;
+}
