9ef8ede986af5a2adfea5504f55947f5a586773b
LibreNovus <vrchat2009@gmail.com>
2020-12-20 01:55:42 -0500

    greedy solution

--- Competitive programming/Codeforces/900/1462c-unique_number.py
+++ Competitive programming/Codeforces/900/1462c-unique_number.py
@@ -2,9 +2,21 @@ from sys import stdin,stdout
 t = int(input())
 
 for y in range(t):
-    x = int(input())
-    sum = 0
-    while (x != 0):
-        sum = sum + x % 10
-        x = x // 10
-    print(sum)
+    n = int(input())
+    if n > 45:
+        string = '-1'
+        print(string)
+        continue
+    else:
+        string = ''
+        i = 9
+        while i >= 1:
+            if n <= 9 and n <= i:
+                string += str(n)
+                n = 0
+                break
+            else:
+                string += str(i)
+                n -= i
+            i -= 1
+    print(string[::-1])
