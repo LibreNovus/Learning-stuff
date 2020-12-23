1de70042cc131242d6c3b3be4169faded9de129f
LibreNovus <vrchat2009@gmail.com>
2020-12-23 09:16:19 -0500

    elephants

---
+++ Competitive programming/Codeforces/800/python/617a-elephant.py
@@ -0,0 +1,22 @@
+x = int(input())
+sum = 0
+while x != 0:
+    if x >= 5:
+        x = x - 5
+        sum += 1
+    elif x >= 4:
+        x = x - 4
+        sum += 1
+    elif x >= 3:
+        x = x - 3
+        sum += 1
+    elif x >= 2:
+        x = x - 2
+        sum += 1
+    elif x >= 2:
+        x = x - 2
+        sum += 1
+    else:
+        x = x -1
+        sum += 1
+print(sum)
